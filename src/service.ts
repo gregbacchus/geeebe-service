62692bcf2e992e7dddceaeeca7b876ec1fd51062
Greg Bacchus <1761608+gregbacchus@users.noreply.github.com>
2018-10-31 14:51:29 +1300

    don't crash on error

--- src/service.ts
+++ src/service.ts
@@ -239,11 +239,8 @@ export abstract class KoaService<TOptions extends ServiceOptions> extends Koa im
    */
   private onError(error: any): void | never {
     log.error(error);
-    if (error.code && typeof error.code === 'string' && error.code.startsWith('HPE_')) {
-      return;
-    }
     if (error.syscall !== 'listen') {
-      throw error;
+      return;
     }
 
     const bind = typeof this.options.port === 'string'
